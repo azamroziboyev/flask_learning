dad3deee1741c79e1b0f3b55e1015ce39533f980
azamroziboyev <aroziboyev07@gmail.com>
2026-07-31 16:30:47 +0500

    day don't know may be 4 learning flask from david malan's course

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
+from flask import Flask, render_template, request
 
-from flask import Flask
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return  "Hello world"
+    text = "Hello"
+    return render_template('base.html', matn=text)
 
 
-if __name__ == __main__:
+@app.route('/indeks')
+def indeks():
+    text = "this is about page"
+    my_list = [10, 20, 30, 40, 50]
+    return render_template('index.html', text=my_list)
+
+@app.template_filter('reversed_string')
+def reverse_string(textcha):
+    return textcha[::-1]
+
+@app.route('/greet')
+def greet():
+    namee = request.args.get("nom", 'world')
+    return render_template('greet.html', ism=namee)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
